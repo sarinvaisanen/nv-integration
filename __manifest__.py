31361b508706f5b49e6d03aa0ecc31afec74d431
Jussi Järviö <jussi.jarvio@reason.fi>
2020-01-15 14:24:48 +0200

    Version up

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Invoicing &amp; Payments',
-    'version': '0.405',
+    'version': '0.406',
 
     # any Odoo module necessary for this one to work correctly
     'depends': ['base', 'account'],
